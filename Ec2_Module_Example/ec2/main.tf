81cf0708e939a0aec44a7d5b4d059c9841e33c1b
Dirie-Hersi <dnhersi@gmail.com>
2022-10-28 03:58:13 +0000

    Updated README

--- Ec2_Module_Example/ec2/main.tf
+++ Ec2_Module_Example/ec2/main.tf
@@ -10,6 +10,6 @@ resource "aws_instance" "app_server" {
   instance_type = var.instance_type
 
   tags = {
-    Name = "ExampleAppServerInstance-${random_integer.random.id}"
+    Name = "ExampleAppServerInstance_${random_integer.random.id}"
   }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Terraform
 
 This is my repo for checking out the cool features of Terraform!
+This is a project I did when learning Terraform modules. Custom EC2 module.
